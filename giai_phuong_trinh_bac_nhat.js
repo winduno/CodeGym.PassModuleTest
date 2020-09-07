c7c8fd6166224bd5f63aaad0f238a6299b0a608c
winduno <windsolo294@gmail.com>
2020-09-07 15:49:56 +0700

    updated

--- giai_phuong_trinh_bac_nhat.js
+++ giai_phuong_trinh_bac_nhat.js
@@ -2,17 +2,19 @@ alert(GiaiPhuongTrinhBacNhat(1, 8));
 
 function GiaiPhuongTrinhBacNhat(num1, num2) {
     if (Number.isInteger(num1) && Number.isInteger(num2)){
-        if (num1 == 0 && num2 != 0){
-            return "Vô nghiệm rồi!";
-        }
-        else if (num1 == 0 && num2 == 0){
-            return "Vô số nghiệm, khỏi cần giải!!"
+        if (num1 == 0){
+           if (num2 == 0){
+               return "Vô số nghiệm, giải thế nào nữa??";
+           }
+           else {
+               return "Vô nghiệm, chịu rồi!!";
+           }
         }
         else {
             return -num2 / num1;
         }
     }
     else {
-        return "Đề bài là nhập 2 số nguyên. 2 số này không nguyên, không giải!!";
+        return "Đề bài là nhập 2 số nguyên. 2 cái nhập vào không phải số nguyên, không giải!!";
     }
 }
